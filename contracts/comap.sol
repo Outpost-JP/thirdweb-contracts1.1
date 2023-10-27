a8e7190850d6f00221089f0eab1df7f354ccf6ef
outpost-caprice <itani@outpost-corp.com>
2023-10-27 10:37:05 +0900

    modified:   contracts/comap.sol 	modified:   package-lock.json 	modified:   package.json

--- contracts/comap.sol
+++ contracts/comap.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC1155Base.sol";
+import "@thirdweb-dev/contracts/extension/Permissions.sol";
 
-contract comap is ERC1155Base {
+contract comap is Permissions, ERC1155Base {
+    bytes32 public constant WHITELISTED_ROLE = keccak256("WHITELISTED_ROLE");  // WHITELISTED_ROLEを定義
 
-      constructor(
+    constructor(
         address _defaultAdmin,
         string memory _name,
         string memory _symbol,
@@ -19,6 +21,35 @@ contract comap is ERC1155Base {
             _royaltyRecipient,
             _royaltyBps
         )
-    {}
+    {
+        // ホワイトリストのロールを設定
+        _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin); // Permissionsで使用するデフォルトの管理者ロール
+    }
 
+    // safeTransferFromをオーバーライド
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public override {
+        require(isEOAorWhitelisted(_msgSender()), "Only EOA or whitelisted addresses allowed");
+        super.safeTransferFrom(from, to, id, amount, data);
+    }
+
+    // safeBatchTransferFromをオーバーライド
+    function safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public override {
+        require(isEOAorWhitelisted(_msgSender()), "Only EOA or whitelisted addresses allowed");
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    // アドレスがEOAまたはホワイトリストに登録されているか確認
+    function isEOAorWhitelisted(address account) internal view returns (bool) {
+        return (tx.origin == account || hasRole(WHITELISTED_ROLE, account));
+    }
+
+    // アドレスをホワイトリストに追加
+    function addToWhitelist(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(WHITELISTED_ROLE, account);
+    }
+
+    // アドレスをホワイトリストから削除
+    function removeFromWhitelist(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(WHITELISTED_ROLE, account);
+    }
 }
