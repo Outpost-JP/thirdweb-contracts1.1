7890f28a745fe5b6e7b99919647d9689436ecd2e
outpost-caprice <itani@outpost-corp.com>
2023-10-27 10:44:10 +0900

    modified:   contracts/comap.sol

--- contracts/comap.sol
+++ contracts/comap.sol
@@ -24,17 +24,17 @@ contract comap is Permissions, ERC1155Base {
     {
         _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
     }
-
+    //関数をオーバーライドしてEOAかWLのアドレスしか呼び出せないようにしている
     function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public override {
         require(isEOAorWhitelisted(msg.sender), "Only EOA or whitelisted addresses allowed");
         super.safeTransferFrom(from, to, id, amount, data);
     }
-
+    //関数をオーバーライドしてEOAかWLのアドレスしか呼び出せないようにしている
     function safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public override {
         require(isEOAorWhitelisted(msg.sender), "Only EOA or whitelisted addresses allowed");
         super.safeBatchTransferFrom(from, to, ids, amounts, data);
     }
-
+    //WLの設定とか諸々
     function isEOAorWhitelisted(address account) internal view returns (bool) {
         return (tx.origin == account || hasRole(WHITELISTED_ROLE, account));
     }
